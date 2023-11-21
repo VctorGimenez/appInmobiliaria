9e128111a33bb34dd940dd070d2780c1a3aa743b
Danna Martinez <62730190+Roumary28@users.noreply.github.com>
2023-11-21 16:40:08 -0300

    proyecto
    Proyecto

---
+++ src/main/java/Uegg/appInmobiliaria/AppInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package Uegg.appInmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppInmobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Uegg/appInmobiliaria/AppInmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package Uegg.appInmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppInmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
